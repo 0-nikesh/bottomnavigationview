2690b42d01d22956f550154966ce8f1265d881a6
nikesh <bhandarinikesh93@gmail.com>
2024-12-23 12:57:40 +0545

    as per sir ko bloc code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
-import 'package:bottomnavigationview/bottom_screen/about.dart';
-import 'package:bottomnavigationview/bottom_screen/cart.dart';
-import 'package:bottomnavigationview/bottom_screen/dashboard.dart';
-import 'package:bottomnavigationview/bottom_screen/profile.dart';
+// import 'package:bottomnavigationview/bottom_screen/about.dart';
+// import 'package:bottomnavigationview/bottom_screen/cart.dart';
+// import 'package:bottomnavigationview/bottom_screen/profile.dart';
+// import 'package:bottomnavigationview/core/app_theme/app_theme.dart';
+// import 'package:bottomnavigationview/view/counter_cubit_view.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//       initialRoute: "/",
+//       theme: getApplicationTheme(),
+//       debugShowCheckedModeBanner: false,
+//       routes: {
+//         // '/': (context) => const DashboardView(),
+//         '/': (context) => const CounterCubitView(),
+//         '/cart': (context) => const CartScreen(),
+//         "/profile": (context) => const ProfilePage(),
+//         "/about": (context) => const AboutScreen(),
+//       }));
+// }
+
+import 'package:bottomnavigationview/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      initialRoute: "/",
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => DashboardView(),
-        '/cart': (context) => CartScreen(),
-        "/profile": (context) => ProfilePage(),
-        "/about": (context) => AboutScreen(),
-      }));
+  runApp(
+    const App(),
+  );
 }
