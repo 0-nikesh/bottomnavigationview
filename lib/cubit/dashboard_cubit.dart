9d71564386e7974808056d4f4a727f051e74ea9b
nikesh <bhandarinikesh93@gmail.com>
2024-12-29 22:57:30 +0545

    added routing for 3 screens

--- lib/cubit/dashboard_cubit.dart
+++ lib/cubit/dashboard_cubit.dart
@@ -1,19 +1,34 @@
+import 'package:bottomnavigationview/cubit/areaofcircle_cubit.dart';
 import 'package:bottomnavigationview/cubit/arithemetic_cubit.dart';
 import 'package:bottomnavigationview/cubit/counter_cubit.dart';
+import 'package:bottomnavigationview/cubit/simpleinterest_cubit.dart';
 import 'package:bottomnavigationview/cubit/student_cubit.dart';
+import 'package:bottomnavigationview/cubit/tsaofcube_cubit.dart';
+import 'package:bottomnavigationview/view/areaofcircle_cubit_view.dart';
 import 'package:bottomnavigationview/view/arithemetic_cubit_view.dart';
 import 'package:bottomnavigationview/view/counter_cubit_view.dart';
+import 'package:bottomnavigationview/view/simpleinterest_view.dart';
 import 'package:bottomnavigationview/view/student_cubit_view.dart';
+import 'package:bottomnavigationview/view/tsaofcube_cubit_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DashboardCubit extends Cubit<void> {
-  DashboardCubit(this._counterCubit, this._arithmeticCubit, this._studentCubit)
-      : super(null);
+  DashboardCubit(
+    this._counterCubit,
+    this._arithmeticCubit,
+    this._studentCubit,
+    this._simpleInterestCubit,
+    this._areaofcircleCubit,
+    this._tsaofcubeCubit,
+  ) : super(null);
 
   final CounterCubit _counterCubit;
   final ArithmeticCubit _arithmeticCubit;
   final StudentCubit _studentCubit;
+  final SimpleInterestCubit _simpleInterestCubit;
+  final AreaofcircleCubit _areaofcircleCubit;
+  final TsaofcubeCubit _tsaofcubeCubit;
 
   void openCounterView(BuildContext context) {
     Navigator.push(
@@ -33,7 +48,19 @@ class DashboardCubit extends Cubit<void> {
       MaterialPageRoute(
         builder: (_) => BlocProvider.value(
           value: _arithmeticCubit,
-          child: ArithmeticCubitView(),
+          child: const ArithmeticCubitView(),
+        ),
+      ),
+    );
+  }
+
+  void openSimpleInterestView(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => BlocProvider.value(
+          value: _simpleInterestCubit,
+          child: const SimpleInterestCubitView(),
         ),
       ),
     );
@@ -46,6 +73,30 @@ class DashboardCubit extends Cubit<void> {
         builder: (_) => BlocProvider.value(
           value: _studentCubit,
           child: StudentCubitView(),
+        ),
+      ),
+    );
+  }
+
+  void openAreaOfCircleView(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => BlocProvider.value(
+          value: _areaofcircleCubit,
+          child: const CircleAreaCubitView(),
+        ),
+      ),
+    );
+  }
+
+  void openTsaOfCube(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => BlocProvider.value(
+          value: _tsaofcubeCubit,
+          child: const CubeTsaCubitView(),
         ),
       ),
     );
