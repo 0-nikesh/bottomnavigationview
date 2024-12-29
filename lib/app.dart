8477678f0c98cac9854590d9ea78cd28c90fe3a0
nikesh <bhandarinikesh93@gmail.com>
2024-12-29 22:56:46 +0545

    add bloc provider of tsa, area of circle and, simple interest

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,10 @@
+import 'package:bottomnavigationview/cubit/areaofcircle_cubit.dart';
 import 'package:bottomnavigationview/cubit/arithemetic_cubit.dart';
 import 'package:bottomnavigationview/cubit/counter_cubit.dart';
 import 'package:bottomnavigationview/cubit/dashboard_cubit.dart';
+import 'package:bottomnavigationview/cubit/simpleinterest_cubit.dart';
 import 'package:bottomnavigationview/cubit/student_cubit.dart';
+import 'package:bottomnavigationview/cubit/tsaofcube_cubit.dart';
 import 'package:bottomnavigationview/view/dashboard_cubit_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,11 +19,17 @@ class App extends StatelessWidget {
         BlocProvider(create: (context) => CounterCubit()),
         BlocProvider(create: (context) => ArithmeticCubit()),
         BlocProvider(create: (context) => StudentCubit()),
+        BlocProvider(create: (context) => AreaofcircleCubit()),
+        BlocProvider(create: (context) => SimpleInterestCubit()),
+        BlocProvider(create: (context) => TsaofcubeCubit()),
         BlocProvider(
             create: (context) => DashboardCubit(
                   context.read<CounterCubit>(),
                   context.read<ArithmeticCubit>(),
                   context.read<StudentCubit>(),
+                  context.read<SimpleInterestCubit>(),
+                  context.read<AreaofcircleCubit>(),
+                  context.read<TsaofcubeCubit>(),
                 ))
       ],
       child: const MaterialApp(
