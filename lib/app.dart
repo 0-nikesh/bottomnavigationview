eae11784a8736b900be23fb5212ea8a47d8cd06e
nikesh <bhandarinikesh93@gmail.com>
2024-12-30 12:59:11 +0545

    normal bloc provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,5 @@
-import 'package:bottomnavigationview/cubit/areaofcircle_cubit.dart';
-import 'package:bottomnavigationview/cubit/arithemetic_cubit.dart';
-import 'package:bottomnavigationview/cubit/counter_cubit.dart';
 import 'package:bottomnavigationview/cubit/dashboard_cubit.dart';
-import 'package:bottomnavigationview/cubit/simpleinterest_cubit.dart';
-import 'package:bottomnavigationview/cubit/student_cubit.dart';
-import 'package:bottomnavigationview/cubit/tsaofcube_cubit.dart';
+import 'package:bottomnavigationview/service_locator/service_locator.dart';
 import 'package:bottomnavigationview/view/dashboard_cubit_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,29 +9,40 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => CounterCubit()),
-        BlocProvider(create: (context) => ArithmeticCubit()),
-        BlocProvider(create: (context) => StudentCubit()),
-        BlocProvider(create: (context) => AreaofcircleCubit()),
-        BlocProvider(create: (context) => SimpleInterestCubit()),
-        BlocProvider(create: (context) => TsaofcubeCubit()),
-        BlocProvider(
-            create: (context) => DashboardCubit(
-                  context.read<CounterCubit>(),
-                  context.read<ArithmeticCubit>(),
-                  context.read<StudentCubit>(),
-                  context.read<SimpleInterestCubit>(),
-                  context.read<AreaofcircleCubit>(),
-                  context.read<TsaofcubeCubit>(),
-                ))
-      ],
-      child: const MaterialApp(
-        // title: 'Flutter BLoC',
-        debugShowCheckedModeBanner: false,
-
-        home: DashboardCubitView(),
+    // return MultiBlocProvider(
+    //   providers: [
+    //     BlocProvider(
+    //       create: (context) => serviceLocator<ArithmeticCubit>(),
+    //     ),
+    //     BlocProvider(
+    //       create: (context) => serviceLocator<StudentCubit>(),
+    //     ),
+    //     BlocProvider(
+    //       create: (context) => serviceLocator<DashboardCubit>(),
+    //     ),
+    //     // BlocProvider(create: (context) => CounterCubit()),
+    //     // BlocProvider(create: (context) => ArithmeticCubit()),
+    //     // BlocProvider(create: (context) => StudentCubit()),
+    //     // // BlocProvider(create: (context) => AreaofcircleCubit()),
+    //     // // BlocProvider(create: (context) => SimpleInterestCubit()),
+    //     // // BlocProvider(create: (context) => TsaofcubeCubit()),
+    //     // BlocProvider(
+    //     //     create: (context) => DashboardCubit(
+    //     //           context.read<CounterCubit>(),
+    //     //           context.read<ArithmeticCubit>(),
+    //     //           context.read<StudentCubit>(),
+    //     //           // context.read<SimpleInterestCubit>(),
+    //     //           // context.read<AreaofcircleCubit>(),
+    //     //           // context.read<TsaofcubeCubit>(),
+    //     //         ))
+    //   ],
+    return MaterialApp(
+      // title: 'Flutter BLoC',
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Bloc",
+      home: BlocProvider.value(
+        value: serviceLocator<DashboardCubit>(),
+        child: const DashboardCubitView(),
       ),
     );
   }
