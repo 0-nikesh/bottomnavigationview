6cd0a41696ce99060f7ef418db8b32334d7a9834
nikesh <bhandarinikesh93@gmail.com>
2024-12-30 12:57:53 +0545

    function added to open bloc of arithemetic

--- lib/cubit/dashboard_cubit.dart
+++ lib/cubit/dashboard_cubit.dart
@@ -1,15 +1,10 @@
-import 'package:bottomnavigationview/cubit/areaofcircle_cubit.dart';
+import 'package:bottomnavigationview/bloc/arithmetic_bloc.dart';
 import 'package:bottomnavigationview/cubit/arithemetic_cubit.dart';
 import 'package:bottomnavigationview/cubit/counter_cubit.dart';
-import 'package:bottomnavigationview/cubit/simpleinterest_cubit.dart';
 import 'package:bottomnavigationview/cubit/student_cubit.dart';
-import 'package:bottomnavigationview/cubit/tsaofcube_cubit.dart';
-import 'package:bottomnavigationview/view/areaofcircle_cubit_view.dart';
 import 'package:bottomnavigationview/view/arithemetic_cubit_view.dart';
 import 'package:bottomnavigationview/view/counter_cubit_view.dart';
-import 'package:bottomnavigationview/view/simpleinterest_view.dart';
 import 'package:bottomnavigationview/view/student_cubit_view.dart';
-import 'package:bottomnavigationview/view/tsaofcube_cubit_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -18,17 +13,19 @@ class DashboardCubit extends Cubit<void> {
     this._counterCubit,
     this._arithmeticCubit,
     this._studentCubit,
-    this._simpleInterestCubit,
-    this._areaofcircleCubit,
-    this._tsaofcubeCubit,
+    this._arithmeticBloc,
+    // this._simpleInterestCubit,
+    // this._areaofcircleCubit,
+    // this._tsaofcubeCubit,
   ) : super(null);
 
   final CounterCubit _counterCubit;
   final ArithmeticCubit _arithmeticCubit;
   final StudentCubit _studentCubit;
-  final SimpleInterestCubit _simpleInterestCubit;
-  final AreaofcircleCubit _areaofcircleCubit;
-  final TsaofcubeCubit _tsaofcubeCubit;
+  final ArithmeticBloc _arithmeticBloc;
+  // final SimpleInterestCubit _simpleInterestCubit;
+  // final AreaofcircleCubit _areaofcircleCubit;
+  // final TsaofcubeCubit _tsaofcubeCubit;
 
   void openCounterView(BuildContext context) {
     Navigator.push(
@@ -54,17 +51,29 @@ class DashboardCubit extends Cubit<void> {
     );
   }
 
-  void openSimpleInterestView(BuildContext context) {
+  void openArtithemeticBlocView(BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (_) => BlocProvider.value(
-          value: _simpleInterestCubit,
-          child: const SimpleInterestCubitView(),
+          value: _arithmeticBloc,
+          child: const ArithmeticCubitView(),
         ),
       ),
     );
   }
+
+  // void openSimpleInterestView(BuildContext context) {
+  //   Navigator.push(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (_) => BlocProvider.value(
+  //         value: _simpleInterestCubit,
+  //         child: const SimpleInterestCubitView(),
+  //       ),
+  //     ),
+  //   );
+  // }
 
   void openStudentView(BuildContext context) {
     Navigator.push(
@@ -78,27 +87,27 @@ class DashboardCubit extends Cubit<void> {
     );
   }
 
-  void openAreaOfCircleView(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => BlocProvider.value(
-          value: _areaofcircleCubit,
-          child: const CircleAreaCubitView(),
-        ),
-      ),
-    );
-  }
+  // void openAreaOfCircleView(BuildContext context) {
+  //   Navigator.push(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (_) => BlocProvider.value(
+  //         value: _areaofcircleCubit,
+  //         child: const CircleAreaCubitView(),
+  //       ),
+  //     ),
+  //   );
+  // }
 
-  void openTsaOfCube(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => BlocProvider.value(
-          value: _tsaofcubeCubit,
-          child: const CubeTsaCubitView(),
-        ),
-      ),
-    );
-  }
+  // void openTsaOfCube(BuildContext context) {
+  //   Navigator.push(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (_) => BlocProvider.value(
+  //         value: _tsaofcubeCubit,
+  //         child: const CubeTsaCubitView(),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
